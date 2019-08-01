9f09307406f888d63648ee2e6020301d41ccac12
Allan Espinosa <allan.espinosa@outlook.com>
2019-08-01 13:00:53 -0400

    Back image directory state in persistent volume

---
+++ src/main/java/io/espinosa/hdfs/kubernetes/PersistentVolumeAsImageDirectory.java
@@ -0,0 +1,91 @@
+package io.espinosa.hdfs.kubernetes;
+
+import io.espinosa.hdfs.ImageDirectory;
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.ApiException;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.models.V1PersistentVolume;
+import io.kubernetes.client.models.V1PersistentVolumeClaim;
+import io.kubernetes.client.models.V1Pod;
+import io.kubernetes.client.models.V1Volume;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class PersistentVolumeAsImageDirectory implements ImageDirectory {
+    public static final String NAMENODE_IMAGE_FORMATTED = "namenode.hdfs.espinosa.io/image-formatted";
+    public static final String IMAGE_VOLUME_NAME = "image";
+    private ApiClient apiClient;
+    private String podName;
+    private String podNameSpace;
+
+    public PersistentVolumeAsImageDirectory(ApiClient client) {
+        this.apiClient = client;
+        this.podNameSpace = "default";
+        this.podName = "namenode-0";
+    }
+
+    @Override
+    public boolean isFormatted() {
+        V1PersistentVolume persistentVolume;
+        try {
+            persistentVolume = getPersistentVolumeFromPod();
+        } catch (ApiException e) {
+            return false;
+        }
+        Map<String, String> annotations = persistentVolume.getMetadata().getAnnotations();
+        if (annotations == null || annotations.isEmpty()) {
+            return false;
+        }
+        return annotations.getOrDefault(NAMENODE_IMAGE_FORMATTED, "false").equals("true");
+    }
+
+    @Override
+    public void skipFormatting() {
+
+    }
+
+    private V1PersistentVolume getPersistentVolumeFromPod() throws ApiException {
+        CoreV1Api api = new CoreV1Api(apiClient);
+        V1Pod pod = api.readNamespacedPod(podName, podNameSpace, null, null, null);
+        Optional<V1Volume> volume = pod.getSpec()
+                .getVolumes()
+                .stream()
+                .filter(
+                        vol -> vol.getName().equals(IMAGE_VOLUME_NAME)
+                )
+                .findFirst();
+        if (!volume.isPresent()) {
+            return null;
+        }
+        String pvcName = volume.get().getPersistentVolumeClaim().getClaimName();
+        V1PersistentVolumeClaim pvc = api.readNamespacedPersistentVolumeClaim(pvcName, podNameSpace, null, null, null);
+        String pvName = pvc.getSpec().getVolumeName();
+        return api.readPersistentVolume(pvName, null, null, null);
+    }
+
+    @Override
+    public void markAsFormatted() {
+        V1PersistentVolume persistentVolume = null;
+        try {
+            persistentVolume = getPersistentVolumeFromPod();
+        } catch (ApiException e) {
+            // TODO: Figure out what is expected here
+        }
+        Map<String, String> annotations = persistentVolume.getMetadata().getAnnotations();
+        if (annotations == null ) {
+            annotations = new HashMap<String, String>();
+        }
+        annotations.put(NAMENODE_IMAGE_FORMATTED, "true");
+        persistentVolume.getMetadata().setAnnotations(annotations);
+
+        CoreV1Api api = new CoreV1Api(apiClient);
+        String pvName = persistentVolume.getMetadata().getName();
+        try {
+            api.replacePersistentVolume(pvName, persistentVolume, null, null);
+        } catch (ApiException e) {
+            // TODO: Figure out what to do when this fails
+        }
+    }
+}
---
+++ src/test/java/io/espinosa/tests/ImageDirectoryIntegrationTest.java
@@ -0,0 +1,140 @@
+package io.espinosa.tests;
+
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import io.espinosa.hdfs.ImageDirectory;
+import io.espinosa.hdfs.kubernetes.PersistentVolumeAsImageDirectory;
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.JSON;
+import io.kubernetes.client.models.*;
+import org.junit.*;
+
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class ImageDirectoryIntegrationTest {
+    private static final int PORT = 8089;
+    public static final String TEST_NAMESPACE = "default";
+    @Rule public WireMockRule stubServer = new WireMockRule(PORT);
+    private JSON json;
+
+    @Before
+    public void initialize() {
+        json = new JSON();
+    }
+
+    @Test
+    public void newPersistentVolumeIsUnformatted() {
+        ImageDirectory imageDirectory = new PersistentVolumeAsImageDirectory(createApiClient());
+
+        V1PersistentVolume pv = new V1PersistentVolumeBuilder()
+                .withNewMetadata()
+                .withName("image-volume")
+                .endMetadata()
+                .build();
+
+        stubPod(TEST_NAMESPACE, pv);
+
+        Assert.assertEquals(false, imageDirectory.isFormatted());
+    }
+
+    @Test
+    public void newPersistentVolumeGetsFormatted() {
+        ImageDirectory imageDirectory = new PersistentVolumeAsImageDirectory(createApiClient());
+
+        V1PersistentVolume pv = new V1PersistentVolumeBuilder()
+                .withNewMetadata()
+                .withName("image-volume")
+                .endMetadata()
+                .build();
+
+        stubPod(TEST_NAMESPACE, pv);
+        stubFor(put("/api/v1/persistentvolumes/image-volume"));
+
+        imageDirectory.markAsFormatted();
+
+        pv.getMetadata().setAnnotations(Map.of(PersistentVolumeAsImageDirectory.NAMENODE_IMAGE_FORMATTED, "true"));
+        verify(
+                putRequestedFor(urlPathEqualTo("/api/v1/persistentvolumes/image-volume"))
+                .withRequestBody(
+                        equalToJson(json.serialize(pv))
+                )
+        );
+    }
+
+    @Test
+    public void annotatedPersistentVolumeIsFormatted() {
+        ImageDirectory imageDirectory = new PersistentVolumeAsImageDirectory(createApiClient());
+
+        V1PersistentVolume pv = new V1PersistentVolumeBuilder()
+                .withNewMetadata()
+                .withName("image-volume")
+                .withAnnotations(Map.of(PersistentVolumeAsImageDirectory.NAMENODE_IMAGE_FORMATTED, "true"))
+                .endMetadata()
+                .build();
+
+        stubPod(TEST_NAMESPACE, pv);
+
+        Assert.assertEquals(true, imageDirectory.isFormatted());
+    }
+
+    private ApiClient createApiClient() {
+        ApiClient client = new ApiClient();
+//        client.setDebugging(true);
+        client.setBasePath(stubServer.baseUrl());
+        return client;
+    }
+
+    private void stubPod(String namespace, V1PersistentVolume pv) {
+        stubFor(
+                get("/api/v1/persistentvolumes/" + pv.getMetadata().getName())
+                        .willReturn(
+                                aJsonResponse(pv)
+                        )
+        );
+
+        V1PersistentVolumeClaim pvc = new V1PersistentVolumeClaimBuilder()
+                .withNewMetadata()
+                .withName("image-namenode-0")
+                .endMetadata()
+                .withNewSpec()
+                .withVolumeName(pv.getMetadata().getName())
+                .endSpec()
+                .build();
+        V1Volume volume = new V1VolumeBuilder()
+                .withName("image")
+                .withNewPersistentVolumeClaim()
+                .withClaimName(pvc.getMetadata().getName())
+                .endPersistentVolumeClaim()
+                .build();
+        V1Pod pod = new V1PodBuilder()
+                .withNewMetadata()
+                .withName("namenode-0")
+                .endMetadata()
+                .withNewSpec()
+                .withVolumes(volume)
+                .endSpec()
+                .build();
+
+
+        stubFor(
+                get("/api/v1/namespaces/" + namespace + "/persistentvolumeclaims/" + pvc.getMetadata().getName())
+                        .willReturn(
+                                aJsonResponse(pvc)
+                        )
+        );
+        stubFor(
+                get("/api/v1/namespaces/" + namespace + "/pods/" + pod.getMetadata().getName())
+                        .willReturn(
+                                aJsonResponse(pod)
+                        )
+        );
+    }
+
+    private ResponseDefinitionBuilder aJsonResponse(Object o) {
+        return aResponse()
+                .withHeader("Content-Type", "application/json")
+                .withBody(json.serialize(o));
+    }
+}

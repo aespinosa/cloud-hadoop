7c58870d4a3840dda9c0772ce1fd036594d99176
Allan Espinosa <allan.espinosa@outlook.com>
2019-08-02 12:59:12 -0400

    Manage cluster's namenode state in statefulset

---
+++ src/main/java/io/espinosa/hdfs/kubernetes/StatefulSetForClusterManagement.java
@@ -0,0 +1,59 @@
+package io.espinosa.hdfs.kubernetes;
+
+import io.espinosa.hdfs.ClusterState;
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.ApiException;
+import io.kubernetes.client.apis.AppsV1Api;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.models.V1Pod;
+import io.kubernetes.client.models.V1StatefulSet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StatefulSetForClusterManagement implements ClusterState {
+    public static final String HDFS_EXISTING_CLUSTER_FLAG = "hdfs.espinosa.io/existing-cluster";
+    private final ApiClient client;
+    private String namespace;
+    private String podName;
+
+    public StatefulSetForClusterManagement(ApiClient client, String namespace, String podName) {
+        this.client = client;
+        this.namespace = namespace;
+        this.podName = podName;
+    }
+
+    @Override
+    public void markAsExistingCluster() throws Exception {
+        V1StatefulSet statefulSet = getStatefulSet();
+        Map<String, String> annotations = statefulSet.getMetadata().getAnnotations();
+        if (annotations == null) {
+            annotations = new HashMap<String, String>();
+        }
+        annotations.put(HDFS_EXISTING_CLUSTER_FLAG, "true");
+        statefulSet.getMetadata().setAnnotations(annotations);
+
+        AppsV1Api api = new AppsV1Api(client);
+        api.replaceNamespacedStatefulSet(statefulSet.getMetadata().getName(), namespace, statefulSet, null, null);
+    }
+
+    @Override
+    public boolean isNewCluster() throws ApiException {
+        V1StatefulSet statefulSet = getStatefulSet();
+        Map<String, String> annotations = statefulSet.getMetadata().getAnnotations();
+        if (annotations == null || annotations.isEmpty()) {
+            return true;
+        }
+        return annotations.getOrDefault(HDFS_EXISTING_CLUSTER_FLAG, "false").equals("false");
+    }
+
+    private V1StatefulSet getStatefulSet() throws ApiException {
+        CoreV1Api api = new CoreV1Api(client);
+        V1Pod pod = api.readNamespacedPod(podName, namespace, null, null, null);
+        // Assumption is that the running pod only has a single ownerReference which is the StatefulSet
+        // responsible for its existence;
+        String statefulSetName = pod.getMetadata().getOwnerReferences().get(0).getName();
+        AppsV1Api appsApi = new AppsV1Api(client);
+        return appsApi.readNamespacedStatefulSet(statefulSetName, namespace, null, null, null);
+    }
+}
---
+++ src/test/java/io/espinosa/tests/ClusterStateIntegrationTest.java
@@ -0,0 +1,117 @@
+package io.espinosa.tests;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import io.espinosa.hdfs.ClusterState;
+import io.espinosa.hdfs.kubernetes.StatefulSetForClusterManagement;
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.JSON;
+import io.kubernetes.client.models.*;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+public class ClusterStateIntegrationTest {
+    private static final String TEST_NAMESPACE = "default";
+    private static final String POD_NAME = "namenode-0";
+    @Rule public WireMockRule stubServer = new WireMockRule(wireMockConfig().dynamicPort());
+
+    @Test
+    public void newCluster() throws Exception {
+        ClusterState state = new StatefulSetForClusterManagement(createApiClient(), TEST_NAMESPACE, POD_NAME);
+
+        V1StatefulSet statefulSet = new V1StatefulSetBuilder()
+                .withKind("StatefulSet")
+                .withNewMetadata()
+                .withName("namenode")
+                .withAnnotations(Map.of("foo", "bar"))
+                .endMetadata()
+                .build();
+        stubPod(statefulSet);
+
+        Assert.assertTrue(state.isNewCluster());
+    }
+
+    @Test
+    public void existingCluster() throws Exception {
+        ClusterState state = new StatefulSetForClusterManagement(createApiClient(), TEST_NAMESPACE, POD_NAME);
+
+        V1StatefulSet statefulSet = new V1StatefulSetBuilder()
+                .withKind("StatefulSet")
+                .withNewMetadata()
+                .withName("namenode")
+                .withAnnotations(Map.of(StatefulSetForClusterManagement.HDFS_EXISTING_CLUSTER_FLAG, "true"))
+                .endMetadata()
+                .build();
+        stubPod(statefulSet);
+
+        Assert.assertFalse(state.isNewCluster());
+    }
+
+    @Test
+    public void markAsExistingCluster() throws Exception {
+        ClusterState state = new StatefulSetForClusterManagement(createApiClient(), TEST_NAMESPACE, POD_NAME);
+
+        Map<String, String> annotations = new HashMap<String, String>();
+        annotations.put("some-other", "annotation");
+
+        V1StatefulSet statefulSet = new V1StatefulSetBuilder()
+                .withKind("StatefulSet")
+                .withNewMetadata()
+                .withAnnotations(annotations)
+                .withName("namenode")
+                .endMetadata()
+                .build();
+        stubPod(statefulSet);
+        stubServer.stubFor(
+                WireMock.put("/apis/apps/v1/namespaces/" + TEST_NAMESPACE + "/statefulsets/namenode")
+        );
+
+        state.markAsExistingCluster();
+
+        annotations.put(StatefulSetForClusterManagement.HDFS_EXISTING_CLUSTER_FLAG, "true");
+
+        stubServer.verify(
+                WireMock.putRequestedFor(WireMock.urlEqualTo("/apis/apps/v1/namespaces/" + TEST_NAMESPACE + "/statefulsets/namenode"))
+        );
+    }
+
+    private ApiClient createApiClient() {
+        ApiClient client = new ApiClient();
+//        client.setDebugging(true);
+        client.setBasePath(stubServer.baseUrl());
+        return client;
+    }
+
+    private void stubPod(V1StatefulSet statefulSet) {
+        JSON json = new JSON();
+        V1OwnerReference ownerStatefulSet = new V1OwnerReferenceBuilder()
+                .withKind(statefulSet.getKind())
+                .withName(statefulSet.getMetadata().getName())
+                .build();
+        V1Pod pod = new V1PodBuilder()
+                .withNewMetadata()
+                .withName("namenode-0")
+                .withOwnerReferences(ownerStatefulSet)
+                .endMetadata()
+                .build();
+
+        stubServer.stubFor(
+                WireMock.get("/apis/apps/v1/namespaces/" + TEST_NAMESPACE + "/statefulsets/namenode")
+                .willReturn(
+                        WireMock.okJson(json.serialize(statefulSet))
+                )
+        );
+        stubServer.stubFor(
+                WireMock.get("/api/v1/namespaces/" + TEST_NAMESPACE + "/pods/" + POD_NAME)
+                .willReturn(
+                        WireMock.okJson(json.serialize(pod))
+                )
+        );
+    }
+}
